4c6b6213c45281ba30d02beefe3e0d025d175eae
Dharshini Saravanan <dharshinisaravanan2604@gmail.com>
2026-08-17 19:47:11 +0530

    Time: 1 ms (97.57%) | Memory: 47.4 MB (41.02%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+        for(int n : nums){
+            total += n;
+        }
+        int ls = 0;
+        for(int i = 0; i < nums.length; i++){
+            int rs = total - ls - nums[i];
+            if( ls == rs){
+                return i;
+            }
+            ls += nums[i];
+        }
+        return -1;
+    }
+}
